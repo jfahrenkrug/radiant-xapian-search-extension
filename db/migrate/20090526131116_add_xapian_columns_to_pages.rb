06ce74d682ecb3405fbc9a894a91bc6ef8557c94
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2009-09-22 08:50:14 +0200

    shorter index name for db2 compatibility

--- db/migrate/20090526131116_add_xapian_columns_to_pages.rb
+++ db/migrate/20090526131116_add_xapian_columns_to_pages.rb
@@ -1,11 +1,11 @@
 class AddXapianColumnsToPages < ActiveRecord::Migration
   def self.up
     add_column :pages, :searchable, :boolean, :default => true
-    add_index  :pages, :searchable
+    add_index  :pages, :searchable, :name => 'xapian_searchable'
   end
 
   def self.down
-    remove_index  :pages, :searchable
+    remove_index  :pages, :name => 'xapian_searchable'
     remove_column :pages, :searchable
   end
 end
