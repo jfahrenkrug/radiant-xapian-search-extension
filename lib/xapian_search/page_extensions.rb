4cb43513d38c304295b23ec6870ccb90e65e6cc5
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2012-07-12 19:02:18 +0200

    keyword support for Radiant 1.0 (extension didn't work with 1.0 at all)

--- lib/xapian_search/page_extensions.rb
+++ lib/xapian_search/page_extensions.rb
@@ -4,7 +4,7 @@ module XapianSearch
    def self.included(base)     
      base.xapit(:conditions => { :searchable => true, :status_id => 100 }) do |index|
            index.text :title, :weight => 10
-           index.text :keywords, :weight => 10
+           index.text :xapian_keywords, :weight => 10
            index.text :description, :weight => 8
            index.text :xapian_parts_content, :weight => 6
            
@@ -17,6 +17,11 @@ module XapianSearch
    # Returns the content of all parts as one string
    def xapian_parts_content
      parts.collect {|p| p.content }.join('\n') 
+   end
+   
+   # Returns the content of all keyword fields as one string
+   def xapian_keywords
+     fields.collect {|f| f.name == "Keywords" ? f.content : ''}.join('\n')
    end
    
    # Returns (if the "site" assoc is present) the page site's or the page parent site's base_domain
